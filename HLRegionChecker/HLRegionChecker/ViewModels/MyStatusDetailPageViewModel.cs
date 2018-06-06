c8e6e4c5505357e93e5f46dac833f7b095203556
aoisupersix <aoi_supersix@yahoo.co.jp>
2018-06-06 17:17:41 +0900

    [add] 詳細ページのバインディングを追加

--- HLRegionChecker/HLRegionChecker/ViewModels/MyStatusDetailPageViewModel.cs
+++ HLRegionChecker/HLRegionChecker/ViewModels/MyStatusDetailPageViewModel.cs
@@ -9,11 +9,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using Xamarin.Forms;
 
 namespace HLRegionChecker.ViewModels
 {
 	public class MyStatusDetailPageViewModel : ViewModelBase
 	{
+        #region プロパティ
         /// <summary>
         /// プロパティの監視管理
         /// </summary>
@@ -25,17 +27,65 @@ namespace HLRegionChecker.ViewModels
         public DbModel Db { get; }
 
         /// <summary>
+        /// ユーザの識別子名(名前)
+        /// </summary>
+        public ReactiveProperty<string> IdentifierName { get; private set; }
+
+        /// <summary>
         /// ユーザのステータステキスト
         /// </summary>
         public ReactiveProperty<string> Status { get; private set; }
+
+        /// <summary>
+        /// ビーコンのUUID
+        /// </summary>
+        public string Beacon_UUID { get; } = "UUID";
+
+        /// <summary>
+        /// ビーコンのメジャー値
+        /// </summary>
+        public string Beacon_Major { get; } = "Major";
+
+        /// <summary>
+        /// ビーコンのマイナー値
+        /// </summary>
+        public string Beacon_Minor { get; } = "Minor";
+
+        /// <summary>
+        /// ビーコンのTxPower
+        /// </summary>
+        public string Beacon_TxPower { get; } = "-59";
+
+        /// <summary>
+        /// アプリアイコン
+        /// </summary>
+        public ImageSource AppIcon { get; } = ImageSource.FromResource("HLRegionChecker.Resources.Icon_AppIcon.png");
+
+        /// <summary>
+        /// アプリのバージョン情報
+        /// </summary>
+        public string Version { get; } = "Version";
+
+        /// <summary>
+        /// アプリのバージョンコード情報
+        /// </summary>
+        public string VersionCode { get; } = "VersionCode";
+        #endregion
 
         /// <summary>
         /// デフォルトのコンストラクタ
         /// </summary>
         public MyStatusDetailPageViewModel(INavigationService navigationService) : base(navigationService)
         {
-            Db = DbModel.Instance;
-            Status = Db.ObserveProperty(x => x.Members).Select(m => Db.GetYourStatusText() ?? "Offline").ToReactiveProperty().AddTo(Disposable);
+            IdentifierName = UserDataModel.Instance.ObserveProperty(x => x.MemberId).Select(id =>
+            {
+                var mems = DbModel.Instance.Members;
+                if (mems.Count <= id)
+                    return "Offline";
+                return mems.Where(m => m.Id == id).Select(m => m.Name).First();
+            }).ToReactiveProperty().AddTo(Disposable);
+
+            Status = DbModel.Instance.ObserveProperty(x => x.Members).Select(m => DbModel.Instance.GetYourStatusText() ?? "Offline").ToReactiveProperty().AddTo(Disposable);
         }
 
         public override void Destroy()
