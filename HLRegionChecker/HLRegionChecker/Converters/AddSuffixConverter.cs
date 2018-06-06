0177c8cb32f20d015ca9096475fee5fb09cf53c7
aoisupersix <aoi_supersix@yahoo.co.jp>
2018-06-06 20:35:16 +0900

    [add] SuffixConverterが抜けていたので追加

---
+++ HLRegionChecker/HLRegionChecker/Converters/AddSuffixConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace HLRegionChecker.Converters
+{
+    /// <summary>
+    /// 値にパラメータに与えられた文字列を追加して返すコンバータクラス
+    /// </summary>
+    public class AddSuffixConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString() + parameter.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
