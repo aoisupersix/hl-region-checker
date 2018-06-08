43c6bd879750901afb71688adf4ed9850ea31411
aoisupersix <aoi_supersix@yahoo.co.jp>
2018-06-08 22:23:11 +0900

    [add] とりあえずAltBeaconの動作確認まで

--- HLRegionChecker/HLRegionChecker.Android/MainActivity.cs
+++ HLRegionChecker/HLRegionChecker.Android/MainActivity.cs
@@ -1,6 +1,11 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using HLRegionChecker.Const;
+using Org.Altbeacon.Beacon;
+using Org.Altbeacon.Beacon.Client;
 using Prism;
 using Prism.Ioc;
 
@@ -18,6 +23,74 @@ namespace HLRegionChecker.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App(new AndroidInitializer()));
+
+            var beaconManager = BeaconManager.GetInstanceForApplication(this.ApplicationContext);
+            beaconManager.BeaconParsers.Add(new BeaconParser().SetBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+            beaconManager.Bind(new MyBeaconConsumer(beaconManager));
+        }
+    }
+
+    public class MyBeaconConsumer : Java.Lang.Object, IBeaconConsumer
+    {
+        private BeaconManager _beaconManager;
+
+        public Context ApplicationContext => throw new System.NotImplementedException();
+
+        public MyBeaconConsumer(BeaconManager manager)
+        {
+            _beaconManager = manager;
+        }
+
+        public bool BindService(Intent p0, IServiceConnection p1, int p2)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnBeaconServiceConnect()
+        {
+            var uuid = Identifier.Parse(RegionConst.BEACON_UUID);
+            var major = Identifier.Parse(RegionConst.BEACON_MAJOR.ToString());
+            var minor = Identifier.Parse(RegionConst.BEACON_MINOR.ToString());
+            var region = new Region("aa", uuid, major, minor);
+
+            _beaconManager.AddMonitorNotifier(new BeaconNotifier());
+            try
+            {
+                _beaconManager.StartMonitoringBeaconsInRegion(region);
+            }
+            catch(Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+            }
+        }
+
+        public void UnbindService(IServiceConnection p0)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class BeaconNotifier : Java.Lang.Object, IMonitorNotifier
+    {
+        //public new IntPtr Handle;
+
+        public void DidDetermineStateForRegion(int p0, Region p1)
+        {
+        }
+
+        public void DidEnterRegion(Region p0)
+        {
+            System.Diagnostics.Debug.WriteLine("Enter");
+        }
+
+        public void DidExitRegion(Region p0)
+        {
+            System.Diagnostics.Debug.WriteLine("Exit");
+        }
+
+        public new void Dispose()
+        {
+            System.Diagnostics.Debug.WriteLine("Dispose");
         }
     }
 
---
+++ HLRegionChecker/HLRegionChecker.Android/Services/BleService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HLRegionChecker.Droid.Services
+{
+    [Service(Name = "org.hykwlab.hlregionchecker.Services.BleService", Process = ":TestProcess")]
+    public class BleService : Service
+    {
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+    }
+}
