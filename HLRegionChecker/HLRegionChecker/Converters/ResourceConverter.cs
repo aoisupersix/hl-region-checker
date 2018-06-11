75b45c349139804330501078a3a7a543cbb2fd1c
aoisupersix <aoi_supersix@yahoo.co.jp>
2018-06-11 21:15:28 +0900

    [add] ResourceConverterを追加

--- HLRegionChecker/HLRegionChecker/Converters/ResourceConverter.cs
+++ HLRegionChecker/HLRegionChecker/Converters/ResourceConverter.cs
@@ -7,16 +7,13 @@ using Xamarin.Forms;
 namespace HLRegionChecker.Converters
 {
     /// <summary>
-    /// メンバーIDからイメージに変換するコンバータクラス
+    /// 引数に与えられた文字列からImageに変換するコンバータクラス
     /// </summary>
-    public class MemberImageConverter: IValueConverter
+    public class ResourceConverter: IValueConverter
     {
-        private static ImageSource memberImage = ImageSource.FromResource("HLRegionChecker.Resources.Icon_Dog.png");
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var memberId = (int)value;
-            //とりあえず全員同じイメージを表示する
-            return memberImage;
+            return ImageSource.FromResource(parameter.ToString());
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
