327b2f0d0352a2e34622c32b705a09cef3ee009c
aoisupersix <aoi_supersix@yahoo.co.jp>
2018-06-16 17:02:50 +0900

    [update] クリーン用スクリプトを改良

--- clean.bat
+++ clean.bat
@@ -6,8 +6,8 @@ SET B1=.\HLRegionChecker\HLRegionChecker\bin\*
 SET O1=.\HLRegionChecker\HLRegionChecker\obj\*
 SET B2=.\HLRegionChecker\HLRegionChecker.Android\bin\*
 SET O2=.\HLRegionChecker\HLRegionChecker.Android\obj\*
-SET B3=.\HLRegionChecker\HLRegionChecker.Android\bin\*
-SET O3=.\HLRegionChecker\HLRegionChecker.Android\obj\*
+SET B3=.\HLRegionChecker\HLRegionChecker.iOS\bin\*
+SET O3=.\HLRegionChecker\HLRegionChecker.iOS\obj\*
 
 echo %DATE% %TIME% èàóùäJén
 
@@ -19,13 +19,13 @@ del /S /Q %O2%
 del /S /Q %B3%
 del /S /Q %O3%
 
-for /D %%1 in (%VS%) do rmdir /S /Q "%%1"
-for /D %%1 in (%B1%) do rmdir /S /Q "%%1"
-for /D %%1 in (%O1%) do rmdir /S /Q "%%1"
-for /D %%1 in (%B2%) do rmdir /S /Q "%%1"
-for /D %%1 in (%O2%) do rmdir /S /Q "%%1"
-for /D %%1 in (%B3%) do rmdir /S /Q "%%1"
-for /D %%1 in (%O3%) do rmdir /S /Q "%%1"
+for /D %%1 in (%VS%) do del /S /Q "%%1"
+for /D %%1 in (%B1%) do del /S /Q "%%1"
+for /D %%1 in (%O1%) do del /S /Q "%%1"
+for /D %%1 in (%B2%) do del /S /Q "%%1"
+for /D %%1 in (%O2%) do del /S /Q "%%1"
+for /D %%1 in (%B3%) do del /S /Q "%%1"
+for /D %%1 in (%O3%) do del /S /Q "%%1"
 
 echo %DATE% %TIME% èàóùèIóπ
 
