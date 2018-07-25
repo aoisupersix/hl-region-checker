554390ba46b5ac4495dc3b498dce979cdbe9dbaa
aoisupersix <aoisupersix@gmail.com>
2018-07-25 21:19:27 +0900

    [del] MainActivityからビーコン関係の処理を削除

--- HLRegionChecker/HLRegionChecker.Android/MainActivity.cs
+++ HLRegionChecker/HLRegionChecker.Android/MainActivity.cs
@@ -1,27 +1,14 @@
-﻿using System;
-using System.Linq;
-using Android.App;
+﻿using Android.App;
 using Android.Content;
 using Android.Content.PM;
 using Android.OS;
-using HLRegionChecker.Const;
-using Org.Altbeacon.Beacon;
-using Org.Altbeacon.Beacon.Client;
 using Firebase;
 using Firebase.Database;
+using Android.Gms.Location;
+using System.Collections.Generic;
+using Android.Gms.Tasks;
 using Prism;
 using Prism.Ioc;
-using Android.Gms.Location;
-using Android.Gms.Common.Apis;
-using Java.Lang;
-using Android.Gms.Common;
-using Android.Views;
-using System.Collections.Generic;
-using Android.Support.V4.App;
-using Android;
-using Android.Support.V4.Content;
-using Android.Gms.Tasks;
-using Xamarin.Forms;
 
 namespace HLRegionChecker.Droid
 {
@@ -39,7 +26,7 @@ namespace HLRegionChecker.Droid
         private GeofencingClient mGeofencingClient;
         private IList<IGeofence> mGeofenceList;
         private PendingIntent mGeofencePendingIntent;
-        private PendingGeofenceTask mPendingGeofenceTask = PendingGeofenceTask.NONE;
+        //private PendingGeofenceTask mPendingGeofenceTask = PendingGeofenceTask.NONE;
 
         public const string PROPERTY_KEY_LOCATION_UPDATES_REQUESTED = "location-updates-requested";
 
@@ -75,10 +62,6 @@ namespace HLRegionChecker.Droid
             mGeofencePendingIntent = null;
             PopulateGeofenceList();
             mGeofencingClient = LocationServices.GetGeofencingClient(this);
-
-            var beaconManager = BeaconManager.GetInstanceForApplication(this.ApplicationContext);
-            beaconManager.BeaconParsers.Add(new BeaconParser().SetBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
-            beaconManager.Bind(new MyBeaconConsumer(beaconManager));
         }
 
         protected override void OnStart()
@@ -178,7 +161,7 @@ namespace HLRegionChecker.Droid
 
         public void OnComplete(Task task)
         {
-            mPendingGeofenceTask = PendingGeofenceTask.NONE;
+            //mPendingGeofenceTask = PendingGeofenceTask.NONE;
             if (task.IsSuccessful)
             {
                 GeofenceAdded = !GeofenceAdded;
@@ -192,69 +175,6 @@ namespace HLRegionChecker.Droid
                 string errorMessage = GeofenceErrorMessages.GetErrorString(this, task.Exception);
                 System.Diagnostics.Debug.WriteLine(errorMessage);
             }
-        }
-    }
-    public class MyBeaconConsumer : Java.Lang.Object, IBeaconConsumer
-    {
-        private BeaconManager _beaconManager;
-
-        public Context ApplicationContext => throw new System.NotImplementedException();
-
-        public MyBeaconConsumer(BeaconManager manager)
-        {
-            _beaconManager = manager;
-        }
-
-        public bool BindService(Intent p0, IServiceConnection p1, int p2)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void OnBeaconServiceConnect()
-        {
-            var uuid = Identifier.Parse(RegionConst.BEACON_UUID);
-            var major = Identifier.Parse(RegionConst.BEACON_MAJOR.ToString());
-            var minor = Identifier.Parse(RegionConst.BEACON_MINOR.ToString());
-            var region = new Region("aa", uuid, major, minor);
-
-            _beaconManager.AddMonitorNotifier(new BeaconNotifier());
-            try
-            {
-                _beaconManager.StartMonitoringBeaconsInRegion(region);
-            }
-            catch (System.Exception e)
-            {
-                System.Diagnostics.Debug.WriteLine(e.Message);
-            }
-        }
-
-        public void UnbindService(IServiceConnection p0)
-        {
-            throw new System.NotImplementedException();
-        }
-    }
-
-    public class BeaconNotifier : Java.Lang.Object, IMonitorNotifier
-    {
-        //public new IntPtr Handle;
-
-        public void DidDetermineStateForRegion(int p0, Region p1)
-        {
-        }
-
-        public void DidEnterRegion(Region p0)
-        {
-            System.Diagnostics.Debug.WriteLine("Enter");
-        }
-
-        public void DidExitRegion(Region p0)
-        {
-            System.Diagnostics.Debug.WriteLine("Exit");
-        }
-
-        public new void Dispose()
-        {
-            System.Diagnostics.Debug.WriteLine("Dispose");
         }
     }
 
