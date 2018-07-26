af597d3cded44cad5d08feb1f22623e3b6ab24e4
aoisupersix <aoisupersix@gmail.com>
2018-07-26 15:56:21 +0900

    [add] Droidの自動更新通知を追加

--- HLRegionChecker/HLRegionChecker.Android/Beacons/BackgroundMonitoringBeaconApplication.cs
+++ HLRegionChecker/HLRegionChecker.Android/Beacons/BackgroundMonitoringBeaconApplication.cs
@@ -22,6 +22,11 @@ namespace HLRegionChecker.Droid
         private RegionBootstrap _regionBootstrap;
         private BackgroundPowerSaver _backgroundPowerSaver;
 
+        /// <summary>
+        /// プッシュ通知を送信します。
+        /// </summary>
+        /// <param name="title">通知タイトル</param>
+        /// <param name="body">通知本文</param>
         private void SendNotification(string title, string body, string info)
         {
             var notificationManager = GetSystemService(NotificationService) as NotificationManager;
@@ -29,14 +34,14 @@ namespace HLRegionChecker.Droid
 
             if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
             {
-                var notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationImportance.High);
+                var notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Update notification of occupancy status", NotificationImportance.High);
 
                 // Configure the notification channel.
-                notificationChannel.Description = "Channel description";
+                notificationChannel.Description = "Update notification of occupancy status";
                 notificationChannel.EnableLights(true);
                 notificationChannel.LightColor = Color.Red;
-                notificationChannel.SetVibrationPattern(new long[]{ 0, 1000, 500, 1000});
-                notificationChannel.EnableVibration(true);
+                //notificationChannel.SetVibrationPattern(new long[]{ 0, 1000, 500, 1000});
+                //notificationChannel.EnableVibration(true);
                 notificationManager.CreateNotificationChannel(notificationChannel);
             }
 
@@ -45,9 +50,9 @@ namespace HLRegionChecker.Droid
 
             notificationBuilder.SetAutoCancel(true)
                 .SetDefaults(1)
-                .SetWhen(100)
-                //.SetSmallIcon(R.drawable.ic_launcher_background)
-                .SetTicker("Hearty365")
+                .SetWhen((long)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalMilliseconds) //タイムスタンプ
+                .SetSmallIcon(Resource.Mipmap.appicon)
+                .SetTicker(body)
                 //.SetPriority(Notification.PRIORITY_MAX)
                 .SetContentTitle(title)
                 .SetContentText(body)
@@ -72,22 +77,48 @@ namespace HLRegionChecker.Droid
             var uuid = Identifier.Parse(RegionConst.BEACON_UUID);
             var major = Identifier.Parse(RegionConst.BEACON_MAJOR.ToString());
             var minor = Identifier.Parse(RegionConst.BEACON_MINOR.ToString());
-            var region = new Org.Altbeacon.Beacon.Region("test", uuid, major, minor);
+            var region = new Org.Altbeacon.Beacon.Region(RegionConst.GetRegionIdentifier(RegionConst.Region.研究室), uuid, major, minor);
             _regionBootstrap = new RegionBootstrap(this, region);
             _backgroundPowerSaver = new BackgroundPowerSaver(this);
         }
 
+        /// <summary>
+        /// ビーコンのステータス更新時のコールバック
+        /// </summary>
+        /// <param name="p0"></param>
+        /// <param name="p1"></param>
         public void DidDetermineStateForRegion(int p0, Org.Altbeacon.Beacon.Region p1)
         {
         }
 
+        /// <summary>
+        /// ビーコン領域に侵入した際のコールバック
+        /// </summary>
+        /// <param name="p0"></param>
         public void DidEnterRegion(Org.Altbeacon.Beacon.Region p0)
         {
-            SendNotification("EnterRegion", "領域に侵入", "test");
+            Console.WriteLine("Enter [{0}] Region", p0.UniqueId);
+
+            if(p0.UniqueId.Equals(RegionConst.GetRegionIdentifier(RegionConst.Region.研究室)))
+            {
+                //研究室に侵入
+                SendNotification("研究室領域に侵入", "ステータスを「在室」に更新しました。", "ステータス自動更新");
+            }
         }
 
+        /// <summary>
+        /// ビーコン領域から退出した際のコールバック
+        /// </summary>
+        /// <param name="p0"></param>
         public void DidExitRegion(Org.Altbeacon.Beacon.Region p0)
         {
+            Console.WriteLine("Exit [{0}] Region", p0.UniqueId);
+
+            if (p0.UniqueId.Equals(RegionConst.GetRegionIdentifier(RegionConst.Region.研究室)))
+            {
+                //研究室から退出
+                SendNotification("研究室領域から退出", "ステータスを「学内」に更新しました。", "ステータス自動更新");
+            }
         }
     }
 }
