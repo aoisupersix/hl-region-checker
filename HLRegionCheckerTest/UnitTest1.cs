c7a4f073118bbbd365a471ef2486c02cdf995504
aoisupersix <aoisupersix@gmail.com>
2018-09-11 15:13:48 +0900

    [add] Unitテストの基盤を追加

---
+++ HLRegionCheckerTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using HLRegionChecker.Converters;
+
+namespace HLRegionCheckerTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var conv = new AddSuffixConverter();
+        }
+    }
+}
