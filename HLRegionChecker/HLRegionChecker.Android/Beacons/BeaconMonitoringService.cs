fa5813f87484fc0fe4847366ebd8b614bdc29fde
aoisupersix <aoisupersix@gmail.com>
2018-10-04 13:20:22 +0900

    [add] BeaconMonitoringServiceを追加

---
+++ HLRegionChecker/HLRegionChecker.Android/Beacons/BeaconMonitoringService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using HLRegionChecker.Droid.Notification;
+using Org.Altbeacon.Beacon;
+
+namespace HLRegionChecker.Droid.Beacons
+{
+    [Service]
+    public class BeaconMonitoringService : Service, IBeaconConsumer
+    {
+        public bool BindService(Intent p0, IServiceConnection p1, int p2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnBeaconServiceConnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IBinder OnBind(Intent intent)
+        {
+            throw new NotImplementedException();
+        }
+
+        [return: GeneratedEnum]
+        public override StartCommandResult OnStartCommand(Intent intent, [GeneratedEnum] StartCommandFlags flags, int startId)
+        {
+            var notification = new Android.App.Notification.Builder(this)
+                .SetContentTitle(Resources.GetString(Resource.String.app_name))
+                .SetContentText("ビーコン領域を監視中")
+                .SetSmallIcon(Resource.Mipmap.appicon)
+                .SetOngoing(true)
+                .Build();
+
+            StartForeground(NotificationUtil.NOTIFICATION_ID, notification);
+
+            return base.OnStartCommand(intent, flags, startId);
+        }
+    }
+}
