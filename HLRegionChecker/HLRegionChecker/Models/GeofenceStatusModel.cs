2a402f1ca00a483376e1ad8c05a4230b98f659b8
aoisupersix <aoi_supersix@yahoo.co.jp>
2018-10-16 16:02:43 +0900

    [add] とりあえずジオフェンス状態モデルを追加

---
+++ HLRegionChecker/HLRegionChecker/Models/GeofenceStatusModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HLRegionChecker.Models
+{
+    /// <summary>
+    /// ジオフェンス状態のモデルクラスです。
+    /// </summary>
+    public class GeofenceStatusModel
+    {
+        /// <summary>
+        /// ジオフェンス識別子（サーバサイド）
+        /// </summary>
+        public string Identifier { get; set; }
+
+        /// <summary>
+        /// 領域に侵入しているか？
+        /// </summary>
+        public bool IsEnter { get; set; }
+    }
+}
